48196fa21d46410b9d68809c0c59ef1a488f68bc
Kedarinath1502 <venkatsaikedarinath@gmail.com>
2025-01-06 12:40:00 -0800

    secondsol

---
+++ 44_goodnodecount.py
@@ -0,0 +1,28 @@
+'''
+Given a binary tree root, a node X in the tree is named good if in the path from root to X 
+there are no nodes with a value greater than X.
+Return the number of good nodes in the binary tree.'''
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        self.count = 0
+        def dfs(root, val):
+            if not root:
+                return
+            if root.val >= val:
+                self.count += 1
+            dfs(root.left, max(root.val, val))
+            dfs(root.right, max(root.val, val ))
+        dfs(root, min(root.val, float('-inf')))
+        return self.count
+    
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(root, val):
+            if not root:
+                return 0
+            res = 1 if root.val >= val else 0
+            res += dfs(root.left, max(root.val, val))
+            res += dfs(root.right, max(root.val, val))
+            return res
+        return dfs(root, root.val)
