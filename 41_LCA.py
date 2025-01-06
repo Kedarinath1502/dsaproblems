5220e3947de6ea183ccdb2f47b42235100554431
Kedarinath1502 <venkatsaikedarinath@gmail.com>
2025-01-05 19:38:40 -0800

    lcawithrecursion

--- 41_LCA.py
+++ 41_LCA.py
@@ -15,6 +15,14 @@ class Solution:
                 curr = curr.right
             else:
                 return curr
-'''
-we can also do it using recursion'''
-            
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if (max(p.val, q.val) < root.val):
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif (min(p.val, q.val) > root.val):
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
