35fbd669e0ebbdb90e5fa21ccef3591e92f00c43
Kedarinath1502 <venkatsaikedarinath@gmail.com>
2025-01-12 18:01:08 -0800

    noofislandsbfs

--- 54_noofislands.py
+++ 54_noofislands.py
@@ -24,3 +24,30 @@ class Solution:
                     islands += 1
         
         return islands
+    
+
+#bfs 
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        rows, cols = len(grid), len(grid[0])
+        islands = 0
+        def bfs(r,c):
+            q = deque()
+            grid[r][c] = "0"
+            q.append((r,c))
+            while q:
+                pr, pc = q.popleft()
+                for nr, nc in directions:
+                    r = pr + nr
+                    c = pc + nc
+                    if (r<0 or c<0 or r>=rows or c>=cols or grid[r][c]=="0"):
+                        continue
+                    q.append((r,c))
+                    grid[r][c] = "0"
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == "1":
+                    bfs(r,c)
+                    islands += 1
+        return islands
